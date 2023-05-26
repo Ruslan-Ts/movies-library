8e395f4b25dc2bf8c452aed885e65cd7b68cd7f6
Ruslan Tsuman <104848743+Ruslan-Ts@users.noreply.github.com>
2023-05-26 12:42:56 +0200

    0

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={ } />
+     
+    </Routes>
+  );
+};
+
+export default App;
